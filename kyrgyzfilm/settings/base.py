1b422b102fb3ccbb20ae0cbc9532cc2f0d756bb1
Kaium Amanbaev <kaiumamanbaev@gmail.com>
2022-09-19 15:27:02 +0600

    add cors

--- kyrgyzfilm/settings/base.py
+++ kyrgyzfilm/settings/base.py
@@ -96,3 +96,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+]
