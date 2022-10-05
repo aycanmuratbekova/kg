f1e050e5e7d9ed7a2439e24ac12d06de7b8a16b1
Amankaium <kaium@list.ru>
2022-10-05 12:33:56 +0600

    add filters and pagination

--- art/views.py
+++ art/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Movie, Person
 from .serializers import MovieSerializer, PersonSerializer
 
@@ -7,8 +7,20 @@ from .serializers import MovieSerializer, PersonSerializer
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        "id", "name", "duration",
+        "year", "genre", "pg_rating", "description",
+        "type", "kind", "footage", "audio",
+        "restoration_year", "trailer"
+    ]
 
 
 class PersonViewSet(ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        "id", "first_name", "second_name",
+        "profession", "bio"
+    ]
--- kyrgyzfilm/settings.py
+++ kyrgyzfilm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
     "corsheaders",
 
     'art',
@@ -130,3 +131,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
