b31aab2ea3237f4b2f0170d56e3961e83b60739e
aycanmuratbekova <aycanmuratbekova@gmail.com>
2023-02-01 02:53:35 +0600

    Renamed app names in django-admin

--- art/apps.py
+++ art/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ArtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'art'
+    verbose_name = "Искусство"
--- studioservices/apps.py
+++ studioservices/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StudioservicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'studioservices'
+    verbose_name = "Сервисы Студии"
