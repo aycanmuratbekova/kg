f59db483bfcf3d24435237e5ce6205b8593bf148
aycanmuratbekova <aycanmuratbekova@gmail.com>
2023-01-31 12:05:50 +0600

    Added new field url_name to Movie, created model for Services

--- studioservices/admin.py
+++ studioservices/admin.py
@@ -4,3 +4,23 @@ from .models import *
 
 admin.site.register(PropsCategory)
 admin.site.register(Props)
+
+
+class ServiceImageAdmin(admin.StackedInline):
+    model = ServiceImage
+    extra = 1
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = [ServiceImageAdmin]
+
+    class Meta:
+        model = Service
+
+
+admin.site.register(ServiceImage)
+admin.site.register(Service, ServiceAdmin)
+
+
+
+
--- studioservices/models.py
+++ studioservices/models.py
@@ -8,8 +8,8 @@ class PropsCategory(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = "Категория реквизитов"
-        verbose_name_plural = "Категории реквизитов"
+        verbose_name = "Категория услуг и реквизитов"
+        verbose_name_plural = "Категории услуг и реквизитов"
 
 
 class Props(models.Model):
@@ -23,3 +23,28 @@ class Props(models.Model):
     class Meta:
         verbose_name = "Реквизит"
         verbose_name_plural = "Реквизиты"
+
+
+class Service(models.Model):
+    category = models.ForeignKey(PropsCategory, models.SET_NULL, 'services', null=True, blank=True, verbose_name="категория услуг")
+    name = models.CharField(max_length=255, verbose_name="Название услуги")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
+
+class ServiceImage(models.Model):
+    service = models.ForeignKey(Service, models.SET_NULL, 'images', null=True, blank=True,
+                                verbose_name="фотографии услуги")
+    img = models.ImageField(upload_to='props/%Y/', verbose_name="Фото")
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Фотография"
+        verbose_name_plural = "Фотографии"
