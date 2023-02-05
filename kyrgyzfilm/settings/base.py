4c7fd4e3962d26361ed4f8b260beb1cd7a211edd
aycanmuratbekova <aycanmuratbekova@gmail.com>
2023-02-06 03:44:26 +0600

    bugfix2

--- kyrgyzfilm/settings/base.py
+++ kyrgyzfilm/settings/base.py
@@ -113,7 +113,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://localhost:3000",
     "https://127.0.0.1:3000",
-    "https://kyrgyz-film.vercel.app/en",
+    "https://kyrgyz-film.vercel.app",
 ]
 
 REST_FRAMEWORK = {
