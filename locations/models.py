eb5445ebafb28b4e500dc4aeae156f514da2ed9e
aycanmuratbekova <aycanmuratbekova@gmail.com>
2023-02-18 20:29:59 +0600

    Wrote a method for compressing a location image

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 
 
@@ -6,6 +7,14 @@ class Location(models.Model):
     url_name = models.CharField(max_length=255, verbose_name="Название латиница")
     location = models.CharField(max_length=255, verbose_name="Местоположение")
     photo = models.ImageField(upload_to="locations", verbose_name="Фото")
+
+    def save(self, *args, **kwargs):
+        super(Location, self).save(*args, **kwargs)
+        if self.photo:
+            img = Image.open(self.photo.path)
+
+            if img.height > 300 or img.width > 300:
+                img.save(self.photo.path, quality=20, optimize=True)
 
     def __str__(self):
         return self.name
